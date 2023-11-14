a3aba5db6e903e6aed2664f9450427d4e683c3c5
nicolette johnson <113725096+aicolette@users.noreply.github.com>
2023-11-13 20:33:40 -0500

    Add files via upload
    step by step guideline of how to do your makeup

---
+++ function practice.py
@@ -0,0 +1,112 @@
+# makeup steps
+def main():
+    startup_message()
+    input("Press Enter for Step 1")
+    step1() 
+    input("Press Enter for Step 2")
+    step2()
+    input("Press Enter for Step 3")
+    step3()
+    input("Press Enter for Step 4")
+    step4()
+    input("Press Enter for Step 5")
+    step5()
+    input("Press Enter for Step 6")
+    step6()
+    input("Press Enter for Step 7")
+    step7()
+    input("Press Enter for Step 8")
+    step8()
+    input("Press Enter for Step 9")
+    step9()
+    input("Press Enter for Step 10")
+    step10()
+    input("Press Enter for Step 11")
+    step11()
+    
+
+
+def startup_message():
+    print("This program is made to help you")    
+    print("learn how to do your makeup")
+    print("in 11 simple steps")
+    print()
+
+def step1():
+    print("Step one: Foundation")
+    print("Dap dots of your foundation on your face")
+    print("Then blend foundation in with brush in circular motions")
+    print("Next dap streaks out with damp sponge")
+    print()
+
+def step2():
+    print("Step two: Contour")
+    print("Apply contour parallel to your tragus but not past your eye")
+    print("Blend out with brush tapping upwards and outwards")
+    print("Be sure to concentrate it within that area around your perimeter to eye")
+    print("Also apply product to corners of forehead, above your brow arch, and below jawline")
+    print("Blend those out in a tapping motion around the perimeter of your face")
+    print()
+
+def step3():
+    print("Step three: Concealer")
+    print("Apply concealer around nose and mouth, on chin, in between eyebrows, and below pupils")
+    print("Blend out concealer on chin, in between eybrows, and around nose and mouth with brush with tapping motions")
+    print("blend out concealer under eyes with damp sponge")
+    print()
+
+def step4():
+    print("Step four: Blush")
+    print("Apply liquid blush underneath pupil, but on apple of cheeck")
+    print("Blend out with brush with light tapping motions going upward diagonally towards perimetier of face.")
+    print("Tap out edges with same sponge used for concealer")
+    print()
+
+def step5():
+    print("Step five: Setting")
+    print("Spray entire face with setting spray and wait until it dries")
+    print("Blend out creases under eye with sponge, then apply setting powder with powder puff ontop")
+    print("Then set areas around mouht, chin, and forehead with the same powder and puff")
+    print()
+
+def step6():
+    print("Step six: Bronzer")
+    print("Apply bronzer with fulffy brush around perimeter of face and tip of nose")
+    print()
+
+def step7():
+    print("Step seven: Blush")
+    print("Apply powder blush where you place your cream/luquid blush with small fluffy brush")
+    print()
+
+def step8():
+    print("Step eight: Eyebrows")
+    print("Shape eyebrows into desired shape")
+    print("Fill them in with pencil, pen, powder, or pomade into desried shape")
+    print("Outline brows with concealer to make shape more defined")
+    print("I like to use a concealer 4 shades lighte runderneath my brows, and one shade lighter on top")
+    print()
+
+def step9():
+    print("Step nine: Eyes")
+    print("Apply conealer on eyes with small fluffy brush")
+    print("Set concealer on eyes with powder and small dense brush")
+    print("Do desired eye look")
+    print()
+
+def step10():
+    print("Step ten: Lips")
+    print("Apply lipliner around perimeter of lips")
+    print("Apply lipstick in center of lips")
+    print ("Apply lip gloss all over lips")
+    print()
+
+def step11():
+    print("Step eleven: Finishing Touches")
+    print("Apply beauty marks with liquid eyeliner if you like")
+    print("Spray face with setting spray until practically drenched")
+    print("Let it dry and ur all done MWAHHHH")
+    print()
+
+
+main()
